933a1499baafc907797727dda220bcaaebfe256f
Allan <workofcode@gmail.com>
2025-01-05 11:48:38 +0000

    - add create/remove for canvas

--- src/js/index.ts
+++ src/js/index.ts
@@ -4,8 +4,8 @@
 import { example } from '../animations/example';
 
 
-const canvas : any = document.getElementById('transitionCanvas');
-const context = canvas.getContext('2d');
+let canvas : HTMLCanvasElement;
+let context;
 
 let winWidth  = window.innerWidth
 let winHeight = window.innerHeight
@@ -13,20 +13,39 @@ let winHeight = window.innerHeight
 // handle event listener here
 window.addEventListener('resize', () => resizeCanvas());
 function resizeCanvas() {
-  winWidth       = window.innerWidth
-  winHeight      = window.innerHeight
-  canvas.width   = winWidth
-  canvas.height  = winHeight
-  canvas.width   = window.innerWidth
-  canvas.height  = window.innerHeight
+  if(canvas){
+    winWidth       = window.innerWidth
+    winHeight      = window.innerHeight
+    canvas.width   = winWidth
+    canvas.height  = winHeight
+    canvas.width   = window.innerWidth
+    canvas.height  = window.innerHeight
+  }
 }
 
 
 // Trigger the animation
-resizeCanvas()
-window.setTimeout(
-  function(){
-    console.log("Start");
-    const animation = example(context, winWidth, winHeight)
-},2000)
+function createCanvas(){
+  if(!canvas){
+    canvas = document.createElement('canvas');
+    canvas.classList.add('ct')
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    context  = canvas.getContext('2d')
+    document.body.appendChild(canvas);
+  }
+}
 
+function removeCanvas(){
+  if(canvas){
+    canvas.remove;
+  }
+}
+
+
+function createTransition(){
+  createCanvas()
+  const animation = example(context, winWidth, winHeight)
+}
+
+createTransition()
